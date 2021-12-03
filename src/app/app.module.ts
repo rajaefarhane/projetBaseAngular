177bf8ba4531bdc5f5911f4eee6cb88272bdc8ea
user.name <r.farhane@codeur.online>
2021-12-03 23:11:05 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ContactsComponent } from './contacts/contacts.component';
+
 import { FormsModule } from '@angular/forms';
 import { AboutService } from 'src/services/about.service';
+import {Routes,RouterModule} from '@angular/router';
 
+const routes:Routes=[
+
+];
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactsComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+   
 
   ],
   providers: [
